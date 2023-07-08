40d46fc1f714aed99335f1d17152241100e97f1d
Iyad Ahmed <iyadahmed430@gmail.com>
2023-07-08 21:45:32 +0300

    Re-use polygon clipping for triangle clipping to reduce code duplication

--- apps/plane_clipping2.cpp
+++ apps/plane_clipping2.cpp
@@ -82,36 +82,9 @@ static std::vector<Vec3> as_polygon([[maybe_unused]] const Triangle &triangle)
                                            const Plane &plane,
                                            std::vector<Triangle> &output)
 {
-  std::vector<Vec3> clipping_result;
-  for (int i = 0; i < 3; i++) {
-    const Vec3 &curr_point = triangle.vertices[(i + 1) % 3];
-    const Vec3 &prev_point = triangle.vertices[i];
-
-    float curr_distance = distance(curr_point, plane);
-    float prev_distance = distance(prev_point, plane);
-
-    if (prev_distance >= 0.0f) {
-      clipping_result.push_back(prev_point);
-    }
-
-    // If there's an intersection between the edge and the plane, add the intersection point
-    // to the clipping result.
-    // Thanks to https://poe.com/GPT-4 for filling this part
-    if ((curr_distance > 0.0f && prev_distance < 0.0f) ||
-        (curr_distance < 0.0f && prev_distance > 0.0f))
-    {
-      float t = prev_distance / (prev_distance - curr_distance);
-      Vec3 intersection_point = prev_point + t * (curr_point - prev_point);
-      clipping_result.push_back(intersection_point);
-    }
-  }
-
-  if (clipping_result.size() == 3) {
-    output.emplace_back(clipping_result[0], clipping_result[1], clipping_result[2]);
-  }
-  else if (clipping_result.size() == 4) {
-    output.emplace_back(clipping_result[0], clipping_result[1], clipping_result[2]);
-    output.emplace_back(clipping_result[2], clipping_result[3], clipping_result[0]);
+  auto clipped_polygon = clip_polygon(as_polygon(triangle), plane);
+  if (!clipped_polygon.empty()) {
+    triangulate_fan(clipped_polygon, output);
   }
 }
 
